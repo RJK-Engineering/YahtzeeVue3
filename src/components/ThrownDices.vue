5e67251ccb51ce11e273d42e14ef930de5bde6a3
RJK <rjklinkhamer@gmail.com>
2024-05-29 12:52:22 +0200

    done for the day

---
+++ src/components/ThrownDices.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="fiveDice">
+        <button id="dice1" class="dice textHover">&#x2680;</button>
+        <button id="dice2" class="dice textHover">&#x2681;</button>
+        <button id="dice3" class="dice textHover">&#x2682;</button>
+        <button id="dice4" class="dice textHover">&#x2683;</button>
+        <button id="dice5" class="dice textHover">&#x2684;</button>
+    </div>
+    <div class="button">
+        <button id="btn" class="btn wHover">Gooien</button>
+    </div>
+</template>
